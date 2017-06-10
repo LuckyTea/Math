1900a3b069fc8dd6ca4662dd03bb7f16a14fc8f7
luckytea <gearpanda@gmail.com>
2017-06-10 08:39:29 +0300

    Fix statements in all tests

--- test.py
+++ test.py
@@ -3,20 +3,21 @@ import main as m
 
 
 class check_answer(unittest.TestCase):
-    def test_correct(self):
+    def test_answer_correct(self):
         answer = 4
         x = 2
         y = 2
         self.assertEqual(m.check_answer(answer,x,y), 1)
-        self.assertEqual(m.get_points(), 1)
-        m.I.points = 0
+        self.assertEqual(m.I.points, 1)
+        m.I.__init__()
 
-    def test_wrong(self):
+    def test_answer_wrong(self):
         answer = 5
         x = 2
         y = 2
         self.assertEqual(m.check_answer(answer,x,y), 0)
-        self.assertEqual(m.get_points(), 0)
+        self.assertEqual(m.I.points, 0)
+        m.I.__init__()
 
 
 class check_timer(unittest.TestCase):
@@ -26,7 +27,7 @@ class check_timer(unittest.TestCase):
         self.assertEqual(m.I.time_left, 0)
         self.assertEqual(m.I.duration, 1)
         self.assertEqual(m.I.game_end, 1)
-        m.Initiation()
+        m.I.__init__()
 
 if __name__ == '__main__':
     unittest.main()
